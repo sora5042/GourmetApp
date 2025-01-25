623efa132f8f56d814a87f513083bd941465ddce
sora5042 <esuora5042@gmail.com>
2025-01-25 20:48:09 +0900

    Initial Commit

---
+++ RamenToCoffee/RamenToCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RamenToCoffeeApp.swift
+//  RamenToCoffee
+//
+//  Created by Sora Oya on 2025/01/25.
+//
+
+import SwiftUI
+
+@main
+struct RamenToCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RamenToCoffeeTests/RamenToCoffeeTests.swift
@@ -0,0 +1,17 @@
+//
+//  RamenToCoffeeTests.swift
+//  RamenToCoffeeTests
+//
+//  Created by Sora Oya on 2025/01/25.
+//
+
+import Testing
+@testable import RamenToCoffee
+
+struct RamenToCoffeeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
